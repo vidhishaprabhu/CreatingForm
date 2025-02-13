f1e38581962f73b4d0275f2d63467a8bd3156094
Vidisha Prabhu B <92219907+vidhishaprabhu@users.noreply.github.com>
2025-02-13 10:39:57 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+<main>
+  
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home />
+
+</main>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
