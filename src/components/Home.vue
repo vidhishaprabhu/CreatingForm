18099dfb5280084e9d7054ae3ad435f9c5991425
Vidisha Prabhu B <92219907+vidhishaprabhu@users.noreply.github.com>
2025-02-17 14:01:02 +0530

    Update Home.vue
    Added ref to my form-project

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,75 +2,75 @@
 <main>
   <h1>Welcome to my Form</h1>
   <p v-if="age > 18"> You are suitable for vote</p>
-  <p v-else-if="age.length == 0">Age should not be blank</p>
+  <p v-else-if="age.length == 0"></p>
   <p v-else>You are not suitable for voting</p>
   <form>
     <div class="mb-3">
-      <label for="exampleInputName" class="form-label">Name</label>
-      <input type="text" class="form-control" id="exampleInputName" placeholder="Enter Name" v-model="name" value="java">
+      <label for="exampleInputName" class="form-label mainlabel">Name</label>
+      <input type="text" class="form-control" id="exampleInputName" placeholder="Enter Name" v-model="name" value="java" ref="inputName" @keydown.enter="handleKeyPress($event, 'inputAge')">
       <span v-if="nameLength" style="color:red">Name should not be blank</span>
-       
+
     </div>
     <div class="mb-3">
-      <label for="exampleInputAge" class="form-label">Age</label>
-      <input type="number" class="form-control" id="exampleInputAge" placeholder="Enter your Age" v-model="age">
+      <label for="exampleInputAge" class="form-label mainlabel">Age</label>
+      <input type="number" class="form-control" id="exampleInputAge" placeholder="Enter your Age" v-model="age" ref="inputAge" @keydown.enter="handleKeyPress($event,'inputEmail')">
       <span v-if="ageLength" style="color:red">Age should not be blank</span>
     </div>
     <div class="mb-3">
-      <label for="exampleInputEmail" class="form-label">Email</label>
-      <input type="email" class="form-control" id="exampleInputEmail" placeholder="Enter Email" v-model="email">
+      <label for="exampleInputEmail" class="form-label mainlabel">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail" placeholder="Enter Email" v-model="email" ref="inputEmail" @keydown="handleKeyPress($event,'inputAddress')">
       <span v-if="emailLength" style="color:red">Email should not be blank</span>
     </div>
     <div class="mb-3">
-      <label for="exampleInputAddress" class="form-label">Address</label>
-      <input type="text" class="form-control" id="exampleInputAddress" placeholder="Enter Address" v-model="address">
+      <label for="exampleInputAddress" class="form-label mainlabel">Address</label>
+      <input type="text" class="form-control" id="exampleInputAddress" placeholder="Enter Address" v-model="address" ref="inputAddress" @keydown="(handleKeyPress($event,'javaCheckbox'))">
       <span v-if="addressLength" style="color:red">Address should not be blank</span>
 
     </div>
-    <label>Skills: </label>
+    <label class="mainlabel">Skills </label>
     <br><br>
     <div class="mb-3 form-check">
       <label class="form-check-label" for="java">Java</label>
-      <input type="checkbox" class="form-check-input" id="java" value="Java" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="java" value="Java" v-model="technology" ref="javaCheckbox" @keydown="(handleKeyPress($event,'htmlCheckbox'))">
 
       <br><br>
       <label class="form-check-label" for="html">HTML</label>
-      <input type="checkbox" class="form-check-input" id="html" value="HTML" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="html" value="HTML" v-model="technology" ref="htmlCheckbox" @keydown="(handleKeyPress($event,'cssCheckbox'))">
       <br><br>
       <label class="form-check-label" for="css">CSS</label>
-      <input type="checkbox" class="form-check-input" id="css" value="CSS" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="css" value="CSS" v-model="technology" ref="cssCheckbox" @keydown="(handleKeyPress($event,'javascriptCheckbox'))">
       <br><br>
       <label class="form-check-label" for="javascript">Javascript</label>
-      <input type="checkbox" class="form-check-input" id="javascript" value="Javascript" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="javascript" value="Javascript" v-model="technology" ref="javascriptCheckbox" @keydown="(handleKeyPress($event,'vuejsCheckbox'))">
       <br><br>
-       <label class="form-check-label" for="vue js">Vue JS</label>
-      <input type="checkbox" class="form-check-input" id="vue js" v-model="technology" value="Vue js">
+      <label class="form-check-label" for="vue js">Vue JS</label>
+      <input type="checkbox" class="form-check-input" id="vue js" v-model="technology" value="Vue js" ref="vuejsCheckbox" @keydown="(handleKeyPress($event,'bootstrapCheckbox'))">
       <br><br>
       <label class="form-check-label" for="bootstrap">Bootstrap</label>
-      <input type="checkbox" class="form-check-input" id="bootstrap" v-model="technology" value="Bootstrap">
+      <input type="checkbox" class="form-check-input" id="bootstrap" v-model="technology" value="Bootstrap" ref="bootstrapCheckbox" @keydown="(handleKeyPress($event,'phpCheckbox'))">
       <br><br>
       <label class="form-check-label" for="php">PHP</label>
-      <input type="checkbox" class="form-check-input" id="php" value="PHP" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="php" value="PHP" v-model="technology" ref="phpCheckbox" @keydown="(handleKeyPress($event,'sqlCheckbox'))">
       <br><br>
       <label class="form-check-label" for="sql" value="SQL">SQL</label>
-      <input type="checkbox" class="form-check-input" id="sql" value="SQL" v-model="technology">
+      <input type="checkbox" class="form-check-input" id="sql" value="SQL" v-model="technology" ref="sqlCheckbox" @keydown="(handleKeyPress($event,'studentRadio'))">
     </div>
-    <label>Type </label>
+    <label class="mainlabel">Type </label>
     <br><br>
     <div class="mb-3 form-check">
       <label class="form-radio-label" for="student">Student</label>
-      <input type="radio" class="form-check-input" id="student" value="Student" v-model="type">
+      <input type="radio" class="form-check-input" id="student" value="Student" v-model="type" ref="studentRadio" @keydown="(handleKeyPress($event,'employeeRadio'))">
       <br><br>
       <label class="form-radio-label" for="employee">Employee</label>
-      <input type="radio" class="form-check-input" id="employee" value="Employee" v-model="type">
+      <input type="radio" class="form-check-input" id="employee" value="Employee" v-model="type" ref="employeeRadio" @keydown="(handleKeyPress($event,'internRadio'))">
       <br><br>
       <label class="form-check-label" for="intern">Intern</label>
-      <input type="radio" class="form-check-input" id="intern" value="Intern" v-model="type">
+      <input type="radio" class="form-check-input" id="intern" value="Intern" v-model="type" ref="internRadio" @keydown="(handleKeyPress($event,'submitbtn'))">
       <br><br>
-  
+
     </div>
     <div class="text-center">
-      <button type="submit" class="btn btn-primary" v-on:click="getData()">Submit</button>
+      <button type="submit" class="btn btn-primary" v-on:click="getData()" refs="submitbtn" ref="submitbtn" id="button">Submit</button>
     </div>
 
     <p>Name: {{name}}</p>
@@ -89,47 +89,127 @@ export default {
   data() {
     return {
       name: "",
-      age:"",
+      age: "",
       email: "",
       address: "",
-      technology:[],
-      type:[],
+      technology: [],
+      type: [],
     }
-   
 
   },
+
   methods: {
+    handleKeyPress(event, nextField) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        if (nextField) {
+          this.$refs[nextField].focus();
+        }
+      }
+    },
     getData() {
-      alert("Name is " + this.name +" and age is "+this.age+" and email is " + this.email + " and address is " + this.address+" and skills are "+this.technology+" and i am a "+this.type);
+      if (!this.name) {
+        this.$refs.inputName.focus();
+        return;
+      }
+      if (!this.age) {
+        this.$refs.inputAge.focus();
+        return;
+      }
+      if (!this.email) {
+        this.$refs.inputEmail.focus();
+        return;
+      }
+      if (!this.address) {
+        this.$refs.inputAddress.focus();
+        return;
+      }
+      if (!this.technology) {
+        
+        if(htmlCheckbox){
+          this.$refs.htmlCheckbox.focus();
+        
+        }
+        if(javaCheckbox)
+        {
+          this.$refs.javaCheckbox.focus();
+        }
+        if(htmlCheckbox)
+        {
+          this.$refs.htmlCheckbox.focus();
+        }
+        if(cssCheckbox)
+        {
+          this.$refs.cssCheckbox.focus();
+        }
+        if(javascriptCheckbox)
+        {
+          this.$refs.javascriptCheckbox.focus();
+        }
+        if(vuejstCheckbox)
+        {
+          this.$refs.vuejsCheckbox.focus();
+        }
+        if(bootstrapCheckbox)
+        {
+          this.$refs.bootstrapCheckbox.focus();
+        }
+        if(phpCheckbox)
+        {
+          this.$refs.phpCheckbox.focus();
+        }
+        if(sqlCheckbox)
+        {
+          this.$refs.sqlCheckbox.focus();
+        }
+
+        return;
+      }
+      if(!this.type){
+        if(studentRadio){
+          this.$refs.studentRadio.focus();
+        }
+        else if(employeeRadio){
+          this.$refs.employeeRadio.focus();
+        }
+        else if(internRadio){
+          this.$refs.internRadio.focus();
+        }
+      }
+      if(submitbtn){
+        this.$refs.submitbtn.focus();
+      }
+
     }
   },
-  computed:{
-    nameLength(){
+  computed: {
+    nameLength() {
       return this.name.length === 0
 
     },
-    ageLength(){
+    ageLength() {
       return this.age.length === 0
     },
-    emailLength(){
+    emailLength() {
       return this.email.length === 0
     },
-    addressLength(){
+    addressLength() {
       return this.address.length === 0
     },
-    skillsLength(){
+    skillsLength() {
       return this.skills.length === 0
-    }
+    },
+
   }
+
 }
 </script>
 
 <style scoped>
-main{
+main {
   background-color: rgb(209, 207, 38);
-  color: white;
+  color: black;
 }
-
 
 h1 {
   text-align: center;
@@ -137,6 +217,17 @@ h1 {
 }
 
 p {
-  color: rgb(115, 155, 215)
+  color: rgb(115, 155, 215);
+  font-size:15px
+}
+label{
+  font-size:25px
+}
+#button{
+  width:250px
+}
+.mainlabel{
+  color:aliceblue;
+  font-weight: 800;
 }
 </style>
